a56a596976c8d761bdf4ece25b7e61907d6e5b13
Kaell <micael.java@outlook.com>
2021-01-10 10:38:42 -0300

    instanciando todos os clientes no App.java

--- src/main/java/com/pix/App.java
+++ src/main/java/com/pix/App.java
@@ -5,20 +5,21 @@ import com.bancocentral.LerClientes;
 
 import com.gui.*;
 
-
 public class App {
-    public static void main( String[] args ){
+    public static void main(String[] args) {
         // JOptionPane
         PixGui gui = new PixGui();
         gui.mostraChave();
 
         // JFrame (usando janelas)
         PixGui2 lababelFrame = new PixGui2();
-        PixGui2 labelFrame = new  PixGui2();
+        PixGui2 labelFrame = new PixGui2();
         labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         labelFrame.setSize(300, 200);
         labelFrame.setVisible(true);
 
-        
+        // Imprime todos os clientes intanciados a partir do arquivo CSV
+        System.out.println(LerClientes.recuperarClientesObj()); // Imprime todos os clientes.
+
     }
 }
