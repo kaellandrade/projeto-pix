d689f863f583fc576a90761f3705424efd8bac5a
Kaell <micael.java@outlook.com>
2021-01-10 10:33:10 -0300

    adicionado o metodo toString na classe ClientePessoaFisica

--- src/main/java/com/pix/ClientePessoaFisica.java
+++ src/main/java/com/pix/ClientePessoaFisica.java
@@ -1,14 +1,21 @@
 package com.pix;
+import com.bancocentral.Pix;
 
 public class ClientePessoaFisica extends Cliente {
     private String cpf;
-    private String dataNascimento;
 
-    ClientePessoaFisica(String nome, String email, String telefone, String chaveString, Conta conta, String cpf,
-            String dataNascimento) {
+    public ClientePessoaFisica(String nome, String email, String telefone, String chaveString, Conta conta, String cpf) {
         super(nome, email, telefone, chaveString, conta);
         this.cpf = cpf;
-        this.dataNascimento = dataNascimento;
+    }
 
+    @Override
+    public String toString() {
+
+        return String.format(
+                "\nNome: %s\nE-mail: %s\nTelefone: %s\nChave Pix: %s\nCPF: %s\n\n*Conta*\nNúmero: %s\nSaldo: %.2fR$\n\nAgencia: %s\n\n*Banco*\nCódigo: %s\nNome: %s\n\n",
+                this.getName(), this.getEmail(), this.getTelefone(), this.getChaveAleatoria(), this.cpf,
+                getConta().getNumero(), getConta().getSaldo(), getConta().getAgencia().getNumeroAgencia(),
+                getConta().getAgencia().getBanco().getCodigoBanco(), getConta().getAgencia().getBanco().getNomeBanco());
     }
 }
