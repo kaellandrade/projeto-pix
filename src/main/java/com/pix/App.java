a263b288544452ae82f5952be9bb7337f7345a5a
Micael Andrade <micael.java@outlook.com>
2021-01-19 08:54:20 -0300

    ajustes no na funçao Main APP

--- src/main/java/com/pix/App.java
+++ src/main/java/com/pix/App.java
@@ -9,6 +9,7 @@ import com.gui.*;
 
 public class App {
     public static void main(String[] args) {
+        /*
         // JOptionPane
         PixGui gui = new PixGui();
         gui.mostraChave();
@@ -19,6 +20,7 @@ public class App {
         labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         labelFrame.setSize(300, 200);
         labelFrame.setVisible(true);
+        */
 
         // Imprime todos os clientes intanciados a partir do arquivo CSV.
 
@@ -35,7 +37,6 @@ public class App {
         // Depois da tranferencia
         System.out.println(a);
         System.out.println(b);
-
         // grava um novo cliente no arquivo CSV
         GravarDados.cadastrarCliente(
                 "PF,CC,Maria santos silva,sil@hotmail.com,00321222107190,+5572909211204,3nl46boi2hasdfasy8bjl535,0066,0031,Andre,456,1212");
