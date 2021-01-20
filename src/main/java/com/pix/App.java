14e9b8bd118227189a18b40adf77cc5a4d3d0a87
Micael Andrade <micael.java@outlook.com>
2021-01-20 20:44:41 -0300

    ajustes de layout no metodo main

--- src/main/java/com/pix/App.java
+++ src/main/java/com/pix/App.java
@@ -1,6 +1,5 @@
 package com.pix;
 
-
 import javax.swing.JFrame;
 import com.banco.*;
 import com.pessoa.*;
@@ -13,7 +12,7 @@ public class App {
     public static void main(String[] args) {
 
         // Imprime todos os clientes intanciados a partir do arquivo CSV.
-        Cliente a = LerClientes.recuperarClientesObj().get(0); //primeiro cliente
+        Cliente a = LerClientes.recuperarClientesObj().get(0); // primeiro cliente
         Cliente b = LerClientes.recuperarClientesObj().get(1); // segundo cliente
 
         // Antes da tranferencia
@@ -26,9 +25,12 @@ public class App {
         // Depois da tranferencia
         System.out.println(a);
         System.out.println(b);
-        
+
         // grava um novo cliente no arquivo CSV
-        GravarDados.cadastrarCliente(
-                "PF,CC,Maria santos silva,sil@hotmail.com,00321222107190,+5572909211204,3nl46boi2hasdfasy8bjl535,0066,0031,Andre,456,1212");
+        /*
+          GravarDados.cadastrarCliente(
+          "PF,CC,Maria santos silva,sil@hotmail.com,00321222107190,+5572909211204,3nl46boi2hasdfasy8bjl535,0066,0031,Andre,456,1212"
+          );
+         */
     }
 }
