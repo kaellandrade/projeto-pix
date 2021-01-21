fbf1fba1d90d51f1a383554510ead119e4ae34cb
Micael Andrade <micael.java@outlook.com>
2021-01-20 21:18:28 -0300

    classe Pix do modulo banco central documentada

--- src/main/java/com/bancocentral/Pix.java
+++ src/main/java/com/bancocentral/Pix.java
@@ -2,17 +2,23 @@ package com.bancocentral;
 
 import java.util.Random;
 
+/**
+ * Pix é responsável pela manutenção das chaves.
+ */
 public class Pix {
     private static final String CARACTERES = "0123456789987654321abcdefghijklmnopqrstuvwxyz";
     private static final int TAMANHO_CHAVE = 20;
 
-    public static String gerarChavePix(){
+    /**
+     * Método responsável por gerar uma chave pix com 20 caracteres alfanuméricos
+     */
+    public static String gerarChavePix() {
         String chave = "";
         int ch;
 
         Random random = new Random();
 
-        for(int i = 0; i < TAMANHO_CHAVE; i++){
+        for (int i = 0; i < TAMANHO_CHAVE; i++) {
             ch = random.nextInt(CARACTERES.length()); // sorteia um caractere da cadeia;
             chave += CARACTERES.charAt(ch);
         }
