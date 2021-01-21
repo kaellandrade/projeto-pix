43d57f511ccfcea9165a7e35ef06bb1e82e3d4ac
Micael Andrade <micael.java@outlook.com>
2021-01-20 21:09:46 -0300

    Ajsute de layout na classe GravarDados

--- src/main/java/com/banco/GravarDados.java
+++ src/main/java/com/banco/GravarDados.java
@@ -5,14 +5,21 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * Classe responsável por gravar um novo cliente no arquivo CSV
+ */
 public class GravarDados {
     private static final String CLIENTES_CSV = "src/main/java/com/banco/clientes.csv";
+
+    /**
+     * Recebe uma string com os dados do cliente em formato CSV e realiza a gravação
+     * no arquivo "clientes.csv"
+     */
     public static boolean cadastrarCliente(String dadosClientes) {
 
         try {
             File file = new File(CLIENTES_CSV);
-            
-            
+
             FileWriter outputfile = new FileWriter(file, true);
             BufferedWriter out = new BufferedWriter(outputfile); // ler o arquivo para er gravado
 
@@ -21,6 +28,7 @@ public class GravarDados {
             out.close();
 
             return true; // cliente salvo com sucesso;
+
         } catch (IOException e) {
             e.printStackTrace();
             return false;
