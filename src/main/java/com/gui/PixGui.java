41c4ca622af7a279e77e60871fdfd1426557aeaf
Micael Andrade <micael.java@outlook.com>
2021-01-20 21:22:35 -0300

    classe PixGui  do modulo banco central documentada

--- src/main/java/com/gui/PixGui.java
+++ src/main/java/com/gui/PixGui.java
@@ -1,9 +1,18 @@
 package com.gui;
+
 import com.bancocentral.Pix;
 import javax.swing.JOptionPane;
 
+/**
+ * PixGui é responsável pela interface gráfica do aplicativo;
+ */
 public class PixGui {
+
+    /**
+     * Mostra chave gerada em uma caixa de diálogo;
+     */
     public void mostraChave() {
-        JOptionPane.showMessageDialog(null, "Sua chave é: " + Pix.gerarChavePix(), "Nova Chave", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Sua chave é: " + Pix.gerarChavePix(), "Nova Chave",
+                JOptionPane.PLAIN_MESSAGE);
     }
 }
