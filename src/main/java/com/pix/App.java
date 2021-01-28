5a6b7ae6666bcc4fa8b31feb9f58f07c54a6db48
Micael Andrade <micael.java@outlook.com>
2021-01-28 11:39:48 -0300

    importacoes pendentes

--- src/main/java/com/pix/App.java
+++ src/main/java/com/pix/App.java
@@ -1,24 +1,25 @@
 package com.pix;
-
+import java.util.ArrayList;
+import com.pessoa.Cliente;
 public class App {
     public static void main(String[] args) {
-        CriaDataBase.abrirArquivo();
-        CriaDataBase.populaDados();
-        CriaDataBase.fechaArquivo();
-        // ArrayList<Cliente> todos_clientes;
+        // CriaDataBase.abrirArquivo();
+        // CriaDataBase.populaDados();
+        // CriaDataBase.fechaArquivo();
+        ArrayList<Cliente> todos_clientes;
 
-        // LerClientesSerializados.abreArquivo();
-        // todos_clientes = LerClientesSerializados.lerClientes();
-        // LerClientesSerializados.fecharArquivo();
+        LerClientesSerializados.abreArquivo();
+        todos_clientes = LerClientesSerializados.lerClientes();
+        LerClientesSerializados.fecharArquivo();
 
         // // Operações aqui
         
-        // System.out.println(todos_clientes.get(0).getConta().getSaldo());
+        System.out.println(todos_clientes.get(0).getConta().getSaldo());
         // System.out.println(todos_clientes.get(1).getConta().getSaldo());
         // System.out.println(todos_clientes.get(2).getConta().getSaldo());
         // // todos_clientes.get(2).getConta().depositar(712);
 
 
-        // LerClientesSerializados.atualizar(todos_clientes); // atualiza o arquivo .pix
+        LerClientesSerializados.atualizar(todos_clientes); // atualiza o arquivo .pix
     }
 }
