bf9ad6ac17c5218f60db2f92ad190457c6625f8c
Breno <breno.barroso@dcomp.ufs.br>
2021-01-27 21:45:29 -0300

    adição de TAXA

--- src/main/java/com/banco/ContaCorrente.java
+++ src/main/java/com/banco/ContaCorrente.java
@@ -1,7 +1,9 @@
 package com.banco;
 
 public class ContaCorrente extends Conta{
-
+    
+    private final float TAXA = 0.02f;
+    
     public ContaCorrente(String numero, float saldo, Agencia agencia){
         super(numero, saldo, agencia);
     }
