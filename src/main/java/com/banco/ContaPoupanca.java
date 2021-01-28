03841dc12e8053c1b61f1fbac566c586a4bbc78d
Micael Andrade <micael.java@outlook.com>
2021-01-28 12:31:01 -0300

    documentação classe ContaPoupanca

--- src/main/java/com/banco/ContaPoupanca.java
+++ src/main/java/com/banco/ContaPoupanca.java
@@ -1,17 +1,19 @@
 package com.banco;
 
-public class ContaPoupanca extends Conta{
+public class ContaPoupanca extends Conta {
 
     private final float TAXA = 0.012f;
-    
+
     public ContaPoupanca(String numero, float saldo, Agencia agencia) {
         super(numero, saldo, agencia);
     }
 
+    /**
+     * Gera uma taxa positiva de 1.2%
+     */
     @Override
     public void gerartaxa() {
         // Implementar...
     }
 
-    
 }
