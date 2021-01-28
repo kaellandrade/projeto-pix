2a91414fc4b9f48c2e7d2e4a7c3770b548b44b12
Manoel Moreira <manoelwp@outlook.com>
2021-01-28 18:14:26 -0300

    Atualização da tela inicial

--- src/main/java/com/gui/TelaInicial.java
+++ src/main/java/com/gui/TelaInicial.java
@@ -11,6 +11,7 @@ public class TelaInicial extends JFrame {
     private JButton buttonTransferencia;
     private JButton buttonExtrato;
     private JLabel labelSaldo;
+    private JButton buttonPix;
 
     public TelaInicial() {
         super("Tela inicial");
@@ -28,16 +29,22 @@ public class TelaInicial extends JFrame {
         labelSaldo.setLocation(200,140);
         c.add(labelSaldo);
 
-        // Botão de transferência
+        // Botão de extrato
         buttonExtrato = new JButton("<html><center>Ver <br /> extrato</center></html>");
         buttonExtrato.setSize(130,50);
         buttonExtrato.setLocation(70,180);
         c.add(buttonExtrato);
 
-        // Botão de extrato
-        buttonTransferencia = new JButton("<html><center>Fazer <br /> transferência</center></html>");
+        // Botão de transferência
+        buttonTransferencia = new JButton("<html><center>Fazer <br /> TEF</center></html>");
         buttonTransferencia.setSize(130,50);
         buttonTransferencia.setLocation(220,180);
         c.add(buttonTransferencia);
+
+        // Botão do PIX
+        buttonPix = new JButton("PIX");
+        buttonPix.setSize(130,50);
+        buttonPix.setLocation(140,245);
+        c.add(buttonPix);
     }
 }
--- src/main/java/com/pix/App.java
+++ src/main/java/com/pix/App.java
@@ -1,12 +1,12 @@
 package com.pix;
 
-import com.gui.TransferenciaGBL;
+import com.gui.TelaInicial;
 import java.text.ParseException;
 import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws ParseException {
-        TransferenciaGBL cadastroTela = new TransferenciaGBL();
+        TelaInicial cadastroTela = new TelaInicial();
         cadastroTela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         cadastroTela.setSize(430, 520);
         cadastroTela.setResizable(false);
