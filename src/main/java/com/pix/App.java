837efea1b8464bd9bc99e5ed98ddf01dc428c624
Micael Andrade <micael.java@outlook.com>
2021-01-28 11:42:52 -0300

    ajustes no método man

--- src/main/java/com/pix/App.java
+++ src/main/java/com/pix/App.java
@@ -14,10 +14,10 @@ public class App {
 
         // // Operações aqui
         
-        System.out.println(todos_clientes.get(0).getConta().getSaldo());
-        // System.out.println(todos_clientes.get(1).getConta().getSaldo());
-        // System.out.println(todos_clientes.get(2).getConta().getSaldo());
-        // // todos_clientes.get(2).getConta().depositar(712);
+        System.out.println(todos_clientes.get(0).getConta().getChavePIX());
+        System.out.println(todos_clientes.get(1).getConta().getSaldo());
+        System.out.println(todos_clientes.get(2).getConta().getSaldo());
+        // todos_clientes.get(2).getConta().depositar(712);
 
 
         LerClientesSerializados.atualizar(todos_clientes); // atualiza o arquivo .pix
