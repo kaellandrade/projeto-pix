446e1ce3a38eda64c03c2effa63a6dbdcf291853
Micael Andrade <micael.java@outlook.com>
2021-01-28 12:28:29 -0300

    documentação Classe ContaCorrente

--- src/main/java/com/banco/ContaCorrente.java
+++ src/main/java/com/banco/ContaCorrente.java
@@ -1,18 +1,25 @@
 package com.banco;
 
-public class ContaCorrente extends Conta{
-    
+public class ContaCorrente extends Conta {
+
     private final float TAXA = 0.02f;
-    
-    public ContaCorrente(String numero, float saldo, Agencia agencia){
+
+    public ContaCorrente(String numero, float saldo, Agencia agencia) {
         super(numero, saldo, agencia);
     }
 
-    public boolean realizarPagamento(ContaSalario conta){
+    /**
+     * Recebe uma conta salário e efetua o pagamento, caso o empregado não tenha recebido
+     * no mes atual
+     */
+    public boolean realizarPagamento(ContaSalario conta) {
         // Implementar...
         return true;
     }
 
+    /*
+     * Para cada movimentação na conta esse método irá gerar uma taxa negativa de 2%;
+     */
     @Override
     public void gerartaxa() {
         // Implementar...
