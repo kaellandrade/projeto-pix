214f2693a06a40b29cf122e20c7fe927b4433a11
Manoel Moreira <manoelwp@outlook.com>
2021-02-03 15:58:16 -0300

    reformulação da interface utilizando GridBagLayout

--- src/main/java/com/gui/NewAbertura.java
+++ src/main/java/com/gui/NewAbertura.java
@@ -1,32 +1,55 @@
 package com.gui;
 
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.GridBagConstraints;
 import java.awt.Dimension;
+
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
 
-public class NewAbertura extends JPanel {
+public class NewAbertura extends JFrame {
+
+    // declarando JTextField
+    JTextField textFieldCPF = new JTextField(11), textFieldCNPJ = new JTextField(14);
     
-    GridBagLayout layout;
+    // declarando JRadioButton
+    JRadioButton radioButtonCPF = new JRadioButton("CPF"), radioButtonCNPJ = new JRadioButton("CNPJ");
+    
+    // declarando JButton
+    JButton buttonEntrar = new JButton("Entrar"), buttonAcessoEspecial = new JButton("Acesso especial");
 
     public NewAbertura() {
-        interfaceGrafica();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setPreferredSize(new Dimension(300,500));
+        JPanel painel1 = new JPanel();
+        painel1.setLayout(new GridBagLayout());
+
+        addElemento(painel1, new JLabel("Selecione a ID"), 0, 0, 1, 1, GridBagConstraints.CENTER);
+        addElemento(painel1, textFieldCPF, 0, 1, 1, 1, GridBagConstraints.CENTER);
+
+        this.add(painel1);
+        this.pack();
+        this.setVisible(true);
     }
 
-    public void interfaceGrafica() {
-        setPreferredSize(new Dimension(300,500));
-        layout = new GridBagLayout();
-        setLayout(layout);
-
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        add(new JLabel("Saldo"), gbc);
-
+    private void addElemento(JPanel p, JComponent c, int x, int y, int largura, int altura, int alinhamento) {
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.gridx = x;
+        gc.gridy = y;
+        gc.gridwidth = largura;
+        gc.gridheight = altura;
+        gc.weightx = 100.0;
+        gc.weighty = 100.0;
+        gc.insets = new Insets(5, 5, 5, 5);
+        gc.anchor = alinhamento;
+        gc.fill = GridBagConstraints.NONE;
+        p.add(c, gc);
     }
 }
--- src/main/java/com/pix/NewApp.java
+++ src/main/java/com/pix/NewApp.java
@@ -2,23 +2,10 @@ package com.pix;
 
 import com.gui.NewAbertura;
 
-import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 public class NewApp extends JFrame {
-
-    public NewApp() {
-        super("Banco XYZ");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setContentPane(new NewAbertura());
-        pack();
-    }
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable(){
-            @Override
-            public void run() {
-                new NewApp().setVisible(true);
-            }
-        });
+        new NewAbertura();
     }
-}
+}       
