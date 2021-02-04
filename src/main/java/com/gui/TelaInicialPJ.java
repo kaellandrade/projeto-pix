e0cddf96a1d7cdca96f53cf0a6efa28d63c613da
Manoel Moreira <manoelwp@outlook.com>
2021-02-04 18:29:35 -0300

    Primeira versão da tela inicial de PJ

--- src/main/java/com/gui/TelaInicialPJ.java
+++ src/main/java/com/gui/TelaInicialPJ.java
@@ -12,17 +12,18 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class TelaInicialPF extends JFrame {
+public class TelaInicialPJ extends JFrame {
 
     // declarando os JLabel
-    JLabel labelHeader = new JLabel("Menu Pessoa Física"), labelSaldo = new JLabel("Saldo");
+    JLabel labelHeader = new JLabel("Menu Pessoa Jurídica"), labelSaldo = new JLabel("Saldo");
 
     // declarando os JButtons
     JButton buttonTI = new JButton("<html><center>Realizar transferência<br />interna</center></html>"),
         buttonExtrato = new JButton("<html><center>Consultar o<br />extrato da conta</center></html>"),
-        buttonPix = new JButton("<html><center>Realizar transferência<br />via Pix</center></html>");
+        buttonPix = new JButton("<html><center>Realizar transferência<br />via Pix</center></html>"),
+        buttonPagamento = new JButton("<html><center>Realizar<br />pagamento</center></html>");
 
-    public TelaInicialPF() {
+    public TelaInicialPJ() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel painel = new JPanel();
         painel.setLayout(new GridBagLayout());
@@ -36,7 +37,8 @@ public class TelaInicialPF extends JFrame {
         // botões de operações
         addElemento(painel, buttonTI, 0, 2, 1, 1, GridBagConstraints.CENTER, 30, 10, 10, 10);
         addElemento(painel, buttonPix, 0, 3, 1, 1, GridBagConstraints.CENTER, 10, 10, 10, 10);
-        addElemento(painel, buttonExtrato, 0, 4, 1, 1, GridBagConstraints.CENTER, 10, 10, 70, 10);
+        addElemento(painel, buttonPagamento, 0, 4, 1, 1, GridBagConstraints.CENTER, 10, 10, 10, 10);
+        addElemento(painel, buttonExtrato, 0, 5, 1, 1, GridBagConstraints.CENTER, 10, 10, 70, 10);
 
         this.add(painel);
         this.pack();
--- src/main/java/com/pix/NewApp.java
+++ src/main/java/com/pix/NewApp.java
@@ -1,12 +1,12 @@
 package com.pix;
 
 import com.gui.Abertura;
-import com.gui.TelaInicialPF;
+import com.gui.TelaInicialPJ;
 
 import javax.swing.JFrame;
 
 public class NewApp extends JFrame {
     public static void main(String[] args) {
-        new TelaInicialPF();
+        new TelaInicialPJ();
     }
 }       
