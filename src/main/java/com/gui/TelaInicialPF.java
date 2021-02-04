edb60fe5d7174ab284a50f89fb908b0f8ea357a0
Manoel Moreira <manoelwp@outlook.com>
2021-02-04 17:52:10 -0300

    Início da reformulação da tela início de PF

---
+++ src/main/java/com/gui/TelaInicialPF.java
@@ -0,0 +1,52 @@
+////////// MUDAR O PREENCHIMENTO DOS BOTÕES
+
+package com.gui;
+
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.GridBagConstraints;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TelaInicialPF extends JFrame {
+
+    // declarando os JLabel
+    JLabel labelHeader = new JLabel("Menu Pessoa Física"), labelSaldo = new JLabel("Saldo");
+
+    // declarando os JButtons
+    JButton buttonTI = new JButton("<html><center>Realizar transferência<br />interna</center></html>"),
+        buttonExtrat = new JButton("Extrato"),
+        buttonPix = new JButton("<html><center>Realizar transferência<br />via Pix</center></html>");
+
+    public TelaInicialPF() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel painel = new JPanel();
+        painel.setLayout(new GridBagLayout());
+
+        // título da tela
+        addElemento(painel, labelHeader, 0, 0, 1, 1, GridBagConstraints.CENTER, 10, 100, 70, 100);
+
+        this.add(painel);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    private void addElemento(JPanel p, JComponent c, int linha, int coluna, int largura, 
+        int altura, int alinhamento, int superior, int esquerda, int inferior, int direita) {
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.gridx = linha;
+        gc.gridy = coluna;
+        gc.gridwidth = largura;
+        gc.gridheight = altura;
+        gc.weightx = 100.0;
+        gc.weighty = 100.0;
+        gc.insets = new Insets(superior, esquerda, inferior, direita);
+        gc.anchor = alinhamento;
+        gc.fill = GridBagConstraints.NONE;
+        p.add(c, gc);
+    }
+}
