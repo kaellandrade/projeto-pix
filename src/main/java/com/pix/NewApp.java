6a11213988625dac78be494f20ec46d6d2d7940d
Manoel Moreira <manoelwp@outlook.com>
2021-02-04 18:47:42 -0300

    Primeira versão da tela de transferência via Pix

--- src/main/java/com/pix/NewApp.java
+++ src/main/java/com/pix/NewApp.java
@@ -1,12 +1,12 @@
 package com.pix;
 
 import com.gui.Abertura;
-import com.gui.TelaInicialPJ;
+import com.gui.TransferenciaPix;
 
 import javax.swing.JFrame;
 
 public class NewApp extends JFrame {
     public static void main(String[] args) {
-        new TelaInicialPJ();
+        new TransferenciaPix();
     }
 }       
