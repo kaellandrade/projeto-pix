654bd9df5497e7f561c798c4696541f10f4fdcfe
kaellandrade <micael.santos@dcomp.ufs.br>
2021-02-05 11:22:39 -0300

    testando pagamentos

--- src/main/java/com/pix/App.java
+++ src/main/java/com/pix/App.java
@@ -4,7 +4,10 @@ import java.util.Map;
 
 import com.bancocentral.Pix;
 import com.pessoa.Cliente;
+import com.pessoa.ClientePessoaFisica;
+import com.pessoa.ClientePessoaJuridica;
 import com.backend.*;
+import com.banco.*;
 
 public class App {
     public static void main(String[] args) {
@@ -19,9 +22,36 @@ public class App {
 
         // TODO: Operações aqui
 
-        // passando um cnpj
-        System.out.println(todos_clientes.get("21637668000114").getConta().getSaldo());
-        System.out.println(todos_clientes.get("21637668000114").getConta().getExtrato());
+        ClientePessoaJuridica empregador =  (ClientePessoaJuridica) todos_clientes.get("21637668000114");
+        ContaCorrente cc = (ContaCorrente) empregador.getConta(); // conta corrente do empregador
+
+        
+        
+        ClientePessoaFisica funcionario =  (ClientePessoaFisica) todos_clientes.get("91365036146");
+        ContaSalario cs = (ContaSalario) funcionario.getConta(); // conta corrente do empregador
+
+
+        System.out.println("Antes do pagamento");
+        System.out.println(cc.getSaldo());
+        System.out.println(cc.getExtrato());
+        System.out.println(cs.getSaldo());
+        System.out.println(cs.getExtrato());
+        
+        // cc.realizarPagamento(cs, 100); // realiza pagamento para uma conta corrente;
+        cc.sacar(100);
+
+
+        System.out.println("Depois do pagamento");
+        System.out.println(cc.getSaldo());
+        System.out.println(cc.getExtrato());
+        System.out.println(cs.getSaldo());
+        System.out.println(cs.getExtrato());
+
+        
+
+
+
+
         LerClientesSerializados.atualizar(todos_clientes.values()); // atualiza o arquivo .pix
     }
 }
