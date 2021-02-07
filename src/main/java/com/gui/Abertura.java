9ba9d71f73bf5a0164bc2f2876aa6e1d563a1b81
kaellandrade <micael.santos@dcomp.ufs.br>
2021-02-07 10:51:35 -0300

    verifica tipo de cliente e captura informacoes

--- src/main/java/com/gui/Abertura.java
+++ src/main/java/com/gui/Abertura.java
@@ -2,12 +2,16 @@ package com.gui;
 
 import com.gui.TelaInicialPF;
 import com.gui.TelaInicialPJ;
+import com.pessoa.Cliente;
+import com.pessoa.ClientePessoaFisica;
+import com.backend.LerClientesSerializados;
 import com.gui.AcessoEspecial;
 
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Map;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.GridBagConstraints;
@@ -29,16 +33,24 @@ public class Abertura extends JFrame {
     // declarando os elementos
     JFormattedTextField idCliente = new JFormattedTextField();
     JButton buttonEntrar = new JButton("Entrar"), buttonAcessoEspecial = new JButton("Acesso especial");
-    
+
     private JRadioButton radioButtonCNPJ, radioButtonCPF;
     private RadioButtonHandler handlerRadioButton;
     private ButtonHandler handlerButtom;
     private ButtonHandlerAE buttonHadlerAE;
     private JPanel painel1;
     private String id;
+    private Map<String, Cliente> todos_clientes;
 
     // Construtor
     public Abertura() {
+        // Início Captura a data base
+
+        LerClientesSerializados.abreArquivo();
+        todos_clientes = LerClientesSerializados.lerClientes();
+        LerClientesSerializados.fecharArquivo();
+        // Fim Captura a data base
+
         painel1 = new JPanel();
         painel1.setLayout(new GridBagLayout());
 
@@ -48,7 +60,7 @@ public class Abertura extends JFrame {
 
         idCliente.setColumns(14);
 
-        // adição dos elementos à interface 
+        // adição dos elementos à interface
         addElemento(painel1, new JLabel("Selecione a ID"), 0, 0, 1, 1, GridBagConstraints.CENTER, 70, 100, 10, 100);
 
         radioButtonCPF = new JRadioButton("CPF", false);
@@ -63,7 +75,7 @@ public class Abertura extends JFrame {
 
         addElemento(painel1, buttonAcessoEspecial, 0, 4, 1, 1, GridBagConstraints.SOUTH, 100, 10, 20, 10);
 
-        //agrupamento dos RadioButtons
+        // agrupamento dos RadioButtons
         ButtonGroup grupo = new ButtonGroup();
         grupo.add(radioButtonCNPJ);
         grupo.add(radioButtonCPF);
@@ -80,8 +92,8 @@ public class Abertura extends JFrame {
     }
 
     // Método que adiciona os elementos
-    private void addElemento(JPanel p, JComponent c, int linha, int coluna, int largura, 
-        int altura, int alinhamento, int superior, int esquerda, int inferior, int direita) {
+    private void addElemento(JPanel p, JComponent c, int linha, int coluna, int largura, int altura, int alinhamento,
+            int superior, int esquerda, int inferior, int direita) {
         GridBagConstraints gc = new GridBagConstraints();
         gc.gridx = linha;
         gc.gridy = coluna;
@@ -133,30 +145,45 @@ public class Abertura extends JFrame {
 
             id = idCliente.getText();
             id = id.replaceAll("\\W", "");
-            
-            if (id.length() == 11) {
-                TelaInicialPF tela = new TelaInicialPF();
-                dispose();
-            }
-
-            else if (id.length() == 14) {
-                TelaInicialPJ tela = new TelaInicialPJ();
-                dispose();
-            }
-
-            else {
-                JOptionPane.showMessageDialog(null, "ID de formato incorreto", "ID de formato incorreto", JOptionPane.ERROR_MESSAGE);
-            }
+            Cliente cli;
+            cli = todos_clientes.get(id);
+            validaCliente(cli);
         }
     }
 
     // Tratamento de evento do botão de acesso especial
     private class ButtonHandlerAE implements ActionListener {
-        
+
         @Override
         public void actionPerformed(ActionEvent evento) {
             AcessoEspecial acessoEspecial = new AcessoEspecial();
             dispose();
+        }
+    }
+    
+    /**
+     * Verifica se o cliente é válido e cria sua respectivaa tela
+     */
+    private void validaCliente(Cliente cli){
+        if (cli != null) {// Cliente válido
+            String nome = cli.getName();
+            String saldo = Float.toString(cli.getConta().getSaldo());
+
+            if (cli instanceof ClientePessoaFisica) {
+                TelaInicialPF telaPF = new TelaInicialPF();
+                telaPF.labelHeader.setText(nome);
+                telaPF.labelSaldo.setText(saldo);
+                dispose();
+            } else {
+                TelaInicialPJ telaPJ = new TelaInicialPJ();
+                telaPJ.labelHeader.setText(nome);
+                telaPJ.labelSaldo.setText(saldo);
+                dispose();
+            }
+
+        } else {
+            JOptionPane.showMessageDialog(null, "ID de formato incorreto", "ID de formato incorreto",
+                    JOptionPane.ERROR_MESSAGE);
         }
     }
 }
