fe745959663c23decd741f5cf928cc58d5937620
kaellandrade <micael.santos@dcomp.ufs.br>
2021-02-07 17:20:03 -0300

    testes saldo dos brancos

--- src/main/java/com/pix/App.java
+++ src/main/java/com/pix/App.java
@@ -6,6 +6,7 @@ import com.bancocentral.Pix;
 import com.bancocentral.PortalTransparencia;
 import com.gui.*;
 import javax.swing.JFrame;
+import javax.swing.text.StyledEditorKit;
 
 import com.pessoa.Cliente;
 import com.pessoa.ClientePessoaFisica;
@@ -15,12 +16,23 @@ import com.banco.*;
 
 public class App {
     public static void main(String[] args) {
-        popularDados();
+        // popularDados();
         Abertura abertura = new Abertura();
         abertura.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         abertura.setVisible(true);
 
+        SaldoBanco sb = new SaldoBanco();
+        sb.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        sb.setVisible(true);
 
+        /*
+        PortalTransparencia a = new PortalTransparencia();
+        System.out.println("Itau:" + a.calcularMontanteItau());
+        System.out.println("Santander:" + a.calcularMontanSantander());
+        System.out.println("Bradesco:" + a.calcularMontanBradesco());
+        System.out.println("Brasil:" + a.calcularMontanteBrasil());
+        */
+        
         // após algumas movimentações rodar esse código abaixo
         // LerClientesSerializados.atualizar(todos_clientes.values()); // atualiza o
         // arquivo .pix
