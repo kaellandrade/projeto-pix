03d276a6959b6a554c52e0128a76f2bcb38aeac7
Manoel Moreira <manoelwp@outlook.com>
2021-02-08 18:49:59 -0300

    Esboço da tela de pagamento

---
+++ src/main/java/com/gui/Pagamento.java
@@ -0,0 +1,27 @@
+// package com.gui;
+
+// import java.awt.GridBagLayout;
+// import java.awt.Insets;
+// import java.awt.GridBagConstraints;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+// import java.text.NumberFormat;
+// import java.util.Locale;
+
+// import javax.swing.JButton;
+// import javax.swing.JComponent;
+// import javax.swing.JLabel;
+// import javax.swing.JPanel;
+// import javax.swing.JTextField;
+
+// import javax.swing.JFrame;
+// import javax.swing.JComboBox;
+// import javax.swing.JMenuBar;
+// import javax.swing.JMenu;
+// import javax.swing.JMenuItem;
+
+// public class Pagamento extends JFrame {
+    
+//     // Declarando o JTextField
+//     JTextField textFieldValor = new JTextField("Valor", 8)
+// }
