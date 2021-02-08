8aa645f82d7b29c14809a727140cf655b55d393b
Manoel Vítor <manoelwp@outlook.com>
2021-02-08 20:58:18 -0300

    Atualização no tamanho dos botões

--- src/main/java/com/gui/TelaPix.java
+++ src/main/java/com/gui/TelaPix.java
@@ -29,7 +29,7 @@ public class TelaPix extends JFrame {
 
     // declarando os elementos
     JLabel labelHeader = new JLabel("Pix");
-    JTextField labelChave = new JTextField("Crie uma chave");
+    JTextField labelChave = new JTextField("Crie uma chave em 'Gerenciar chaves'");
     JButton buttonTransferir = new JButton("Realizar transferência"), buttonExtrato = new JButton("Extrato"),
             buttonGenChaves = new JButton("Gerenciar chaves");
 
@@ -76,14 +76,11 @@ public class TelaPix extends JFrame {
         // bHandlerGenChaves = BHandlerGenChaves();
 
         // Adição dos elementos
-        addElemento(painel, labelHeader, 1, 0, GridBagConstraints.CENTER, 10, 150, 40, 150, GridBagConstraints.NONE);
-        addElemento(painel, labelChave, 1, 1, GridBagConstraints.CENTER, 10, 10, 40, 10, GridBagConstraints.NONE);
-        addElemento(painel, buttonTransferir, 1, 2, GridBagConstraints.CENTER, 10, 60, 20, 60,
-                GridBagConstraints.HORIZONTAL);
-        addElemento(painel, buttonExtrato, 1, 3, GridBagConstraints.CENTER, 10, 60, 20, 60,
-                GridBagConstraints.HORIZONTAL);
-        addElemento(painel, buttonGenChaves, 1, 4, GridBagConstraints.CENTER, 10, 60, 40, 60,
-                GridBagConstraints.HORIZONTAL);
+        addElemento(painel, labelHeader, 1, 0, GridBagConstraints.CENTER, 10, 150, 40, 150, GridBagConstraints.NONE, 10);
+        addElemento(painel, labelChave, 1, 1, GridBagConstraints.CENTER, 10, 10, 40, 10, GridBagConstraints.NONE, 10);
+        addElemento(painel, buttonTransferir, 1, 2, GridBagConstraints.CENTER, 10, 60, 20, 60, GridBagConstraints.HORIZONTAL, 10);
+        addElemento(painel, buttonExtrato, 1, 3, GridBagConstraints.CENTER, 10, 60, 20, 60, GridBagConstraints.HORIZONTAL, 10);
+        addElemento(painel, buttonGenChaves, 1, 4, GridBagConstraints.CENTER, 10, 60, 40, 60, GridBagConstraints.HORIZONTAL, 10);
 
         // adiciona menu ao JFrame
         menu.add(login);
@@ -104,14 +101,15 @@ public class TelaPix extends JFrame {
     }
 
     // Método que adiciona os elementos à interface
-    private void addElemento(JPanel p, JComponent c, int coluna, int linha, int alinhamento, int superior, int esquerda,
-            int inferior, int direita, int preenchimento) {
+    private void addElemento(JPanel p, JComponent c, int coluna, int linha, int alinhamento, 
+        int superior, int esquerda, int inferior, int direita, int preenchimento, int ipady) {
 
         GridBagConstraints gc = new GridBagConstraints();
         gc.gridx = coluna;
         gc.gridy = linha;
         gc.gridwidth = 1;
         gc.gridheight = 1;
+        gc.ipady = ipady;
         gc.insets = new Insets(superior, esquerda, inferior, direita);
         gc.anchor = alinhamento;
         gc.fill = preenchimento;
