412de5ccde2e9b13ef5a8e22221bbb8928587b38
Manoel Vítor <manoelwp@outlook.com>
2021-02-08 22:21:46 -0300

    Adição de menu

--- src/main/java/com/gui/SaldoBanco.java
+++ src/main/java/com/gui/SaldoBanco.java
@@ -2,18 +2,23 @@ package com.gui;
 
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.awt.GridBagConstraints;
 
-import javax.swing.JPanel;
-
 import com.bancocentral.PortalTransparencia;
 
+import javax.swing.JPanel;
+import javax.swing.event.AncestorListener;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class SaldoBanco extends JFrame {
     PortalTransparencia transparencia = new PortalTransparencia();
@@ -30,28 +35,48 @@ public class SaldoBanco extends JFrame {
     String bancos[] = {"Banco do Brasil", "Bradesco", "Itaú", "Santander"};
     JComboBox selecaoBanco = new JComboBox(bancos);
 
+    // Declarando o JMenu, JMenuBar e JMenuItem;
+    private JMenuBar barra;
+    private JMenu menu;
+    private JMenuItem login;
+
+    // Variáveis para tratamento de eventos do menu
+    private BHandlerLogin bHandlerLogin;
+
     public SaldoBanco() {
+       
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel painel = new JPanel();
+        painel.setLayout(new GridBagLayout());
+
+        // Define o menu e seus componentes
+        barra = new JMenuBar();
+        menu = new JMenu("Opções");
+        login = new JMenuItem("Tela de login");
+        bHandlerLogin = new BHandlerLogin();
+
         // Testando para o banco do brasil
         ArrayList<Float> total = new ArrayList<>();
         labelSaldoCC.setText(Float.toString(transparencia.calcularMontanteBrasil().get("corrente")));
         labelSaldoPP.setText(Float.toString(transparencia.calcularMontanteBrasil().get("poupanca")));
         labelSaldoCS.setText(Float.toString(transparencia.calcularMontanteBrasil().get("salario")));
 
-        
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel painel = new JPanel();
-        painel.setLayout(new GridBagLayout());
-
-        // adição dos JLabel
+        // Adição dos elementos
         addElemento(painel, labelH1, 0, 0, 1, 1, GridBagConstraints.CENTER, 10, 100, 30, 100);
         addElemento(painel, labelH2, 0, 1, 1, 1, GridBagConstraints.WEST, 10, 10, 3, 10);
         addElemento(painel, labelSaldoCC, 0, 3, 1, 1, GridBagConstraints.CENTER, 40, 10, 50, 10);
         addElemento(painel, labelSaldoPP, 0, 4, 1, 1, GridBagConstraints.CENTER, 40, 10, 50, 10);
         addElemento(painel, labelSaldoCS, 0, 5, 1, 1, GridBagConstraints.CENTER, 40, 10, 50, 10);
         addElemento(painel, labelSaldoGeral, 0, 6, 1, 1, GridBagConstraints.CENTER, 40, 10, 50, 10);
-
-        // adição do JComboBox
         addElemento(painel, selecaoBanco, 0, 2, 1, 1, GridBagConstraints.WEST, 3, 10, 10, 10);
+
+        // adiciona menu ao JFrame
+        menu.add(login);
+        barra.add(menu);
+        this.setJMenuBar(barra);
+
+        // Tratamento de eventos
+        login.addActionListener(bHandlerLogin);
 
         this.add(painel);
         this.pack();
@@ -73,4 +98,15 @@ public class SaldoBanco extends JFrame {
         gc.fill = GridBagConstraints.NONE;
         p.add(c, gc);
     }
+
+    // Tratamento de eventos do menu (login)
+    private class BHandlerLogin implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent evento) {
+            Abertura abertura = new Abertura();
+            dispose();
+        }
+    }
+    
 }
